cd0d38240c310f6dab7528f7d287eda1ebb48b21
Onur Kılınç <onur81730@gmail.com>
2025-12-02 01:39:32 +0300

    Ürün Stok Takip projesi

---
+++ UrunStockTakip/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UrunStokTakip.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ürün adı zorunludur.")]
+        [StringLength(50, ErrorMessage = "Ürün adı en fazla 50 karakter olabilir.")]
+        public string Name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stok 0'dan küçük olamaz.")]
+        public int Stock { get; set; }
+
+        [Range(0.01, 999999, ErrorMessage = "Fiyat 0'dan büyük olmalıdır.")]
+        [Column(TypeName = "decimal(18,2)")] // SQL decimal hatasını önler
+        public decimal Price { get; set; }
+    }
+}
---
+++ UrunStockTakip/data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
